c6a89263efb9fcaf802a431fba07714757c77182
Pablo Kalmychetty <adi-dasler@lenta.ru>
2020-05-05 11:57:09 +0300

    Delivery food day1

---
+++ js/main.js
@@ -0,0 +1,82 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle('is-open');
+}
+
+// day 1
+
+const buttonAuth = document.querySelector('.button-auth'),
+      modalAuth = document.querySelector('.modal-auth'),
+      closeAuth = document.querySelector('.close-auth'),
+      logInForm = document.getElementById('logInForm'),
+      loginInput = document.getElementById('login'),
+      userName = document.querySelector('.user-name'),
+      buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('gloDelivery');
+
+      function toogleModalAuth() {
+        modalAuth.classList.toggle('is-open');
+      }
+
+function autorized() {
+
+  function logOut() {
+    login = null;
+    localStorage.removeItem('gloDelivery');
+    buttonAuth.style.display = '';
+    userName.style.display = ''; 
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+
+  console.log('yes');
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline'; 
+  buttonOut.style.display = 'block';
+  buttonOut.addEventListener('click', logOut);
+}
+
+function notAutorized() {
+  console.log('no');
+
+function logIn(event) {
+  event.preventDefault();
+  if(login = loginInput.value) {
+    localStorage.setItem('gloDelivery', login);
+    toogleModalAuth();
+    buttonAuth.removeEventListener('click', toogleModalAuth);
+    closeAuth.removeEventListener('click', toogleModalAuth);
+    logInForm.removeEventListener('submit', logIn);
+    logInForm.reset();
+  checkAuth();
+  } else {
+      alert('Желательно ввести логин и поскорее')
+  }
+  
+}
+
+  buttonAuth.addEventListener('click', toogleModalAuth);
+  closeAuth.addEventListener('click', toogleModalAuth);
+  logInForm.addEventListener('submit', logIn);
+}
+
+function checkAuth() {
+  if(login) {
+    autorized();
+  } else {
+    notAutorized();
+  }
+}
+
+checkAuth();
